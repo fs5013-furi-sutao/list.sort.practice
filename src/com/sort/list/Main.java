da67fb50f8f599f3275d43f5e6b8314999cf02ab
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2020-06-05 18:41:31 +0900

    Change the Main class for cutting out static methods

--- src/com/sort/list/Main.java
+++ src/com/sort/list/Main.java
@@ -5,28 +5,40 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         // 従業員一覧を作成
-        List<Employee> emps = new ArrayList<Employee>();
-        emps.add(new Employee().id(1).firstName("Yamada").lastName("Taro").age(35).salary(5000));
-        emps.add(new Employee().id(3).firstName("Yamamoto").lastName("Ichiro").age(15).salary(120000));
-        emps.add(new Employee().id(2).firstName("Asai").lastName("Asao").age(18).salary(1000));
+        List<Employee> emps = makeEmps();
 
         System.out.println("並び替え前:");
-        Employee.showAllEmpsWithProps(emps);
+        out(emps);
 
         System.out.println();
         System.out.println("年齢順に並び替え：");
         List<Employee> empsSortedByAgeAsc = Employee.sortByAgeAsc(emps);
-        Employee.showAllEmpsWithProps(empsSortedByAgeAsc);
+        out(empsSortedByAgeAsc);
 
-        
+
         System.out.println();
         System.out.println("名前順に並び替え：");
         List<Employee> empsSortedByNameAsc = Employee.sortByNameAsc(emps);
-        Employee.showAllEmpsWithProps(empsSortedByNameAsc);
+        out(empsSortedByNameAsc);
 
+        System.out.println();
         System.out.println("並び替え前:");
+        out(emps);
+    }
+
+    // 従業員一覧を作成
+    private static List<Employee> makeEmps() {
+        List<Employee> emps = new ArrayList<Employee>();
+        emps.add(new Employee().id(1).firstName("Yamada").lastName("Taro").age(35).salary(5000));
+        emps.add(new Employee().id(3).firstName("Yamamoto").lastName("Ichiro").age(15).salary(120000));
+        emps.add(new Employee().id(2).firstName("Asai").lastName("Asao").age(18).salary(1000));
+        return emps;
+    }
+
+    // 委譲で従業員リストを一覧表示
+    private static void out(final List<Employee> emps) {
         Employee.showAllEmpsWithProps(emps);
     }
 }
